a4d42e1acb8ad9ac1143122f3da9015c0a59be19
meiyaz <54905082+Mr-Meiy@users.noreply.github.com>
2020-01-22 11:35:00 +0530

    transfer working

--- NetBanking.java
+++ NetBanking.java
@@ -198,9 +198,59 @@ public class NetBanking extends JFrame implements ActionListener {
 
     private JPanel createTransferPanel() {
         JPanel p = new JPanel(null);
-        JLabel l = new JLabel("Transfer Cash (Coming Soon)");
-        l.setBounds(100, 100, 300, 30);
-        p.add(l);
+        
+        JLabel lTitle = new JLabel("BALANCE TRANSFER");
+        lTitle.setFont(new Font("Arial", Font.BOLD, 20));
+        lTitle.setBounds(300, 30, 250, 30);
+        p.add(lTitle);
+
+        JLabel lBal = new JLabel("CURRENT BALANCE :");
+        lBal.setBounds(100, 100, 150, 30);
+        p.add(lBal);
+        
+        JLabel lCurrentBal = new JLabel("Rs. " + currentUser.balance);
+        lCurrentBal.setBounds(300, 100, 200, 30);
+        p.add(lCurrentBal);
+
+        JLabel lAcc = new JLabel("PARTNER'S ACC NO:");
+        lAcc.setBounds(100, 150, 150, 30);
+        p.add(lAcc);
+        
+        JTextField tAcc = new JTextField();
+        tAcc.setBounds(300, 150, 200, 30);
+        p.add(tAcc);
+
+        JLabel lAmt = new JLabel("AMOUNT TO TRANSFER :");
+        lAmt.setBounds(100, 200, 150, 30);
+        p.add(lAmt);
+        
+        JTextField tAmt = new JTextField();
+        tAmt.setBounds(300, 200, 200, 30);
+        p.add(tAmt);
+        
+        JButton btnSubmit = new JButton("TRANSFER");
+        btnSubmit.setBounds(250, 300, 150, 30);
+        p.add(btnSubmit);
+
+        btnSubmit.addActionListener(e -> {
+            try {
+                double amount = Double.parseDouble(tAmt.getText());
+                if (amount <= 0) {
+                     JOptionPane.showMessageDialog(this, "Invalid Amount");
+                } else if (amount > currentUser.balance) {
+                    JOptionPane.showMessageDialog(this, "Insufficient Balance");
+                } else {
+                    currentUser.balance -= amount;
+                    lCurrentBal.setText("Rs. " + currentUser.balance);
+                    JOptionPane.showMessageDialog(this, "Transfer Successful! New Balance: " + currentUser.balance);
+                    tAmt.setText("");
+                    tAcc.setText("");
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this, "Please enter a valid number");
+            }
+        });
+
         addBackButton(p);
         return p;
     }
